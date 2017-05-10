ecc85b794cc77a6723810144830a9b03055694ab
Harshith <harshith.durgaprasad@citrix.com>
2017-05-10 19:20:40 +0530

    java-checkin

---
+++ JavaProjects/src/com/citrix/basics/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.citrix.basics;
+
+/**
+ * Created by admin on 10/05/17.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
